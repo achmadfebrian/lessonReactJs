635c99b4bc165bfa92e98ced0f8976fce60780f9
Achmad Febrian <39165413+achmadfebrian@users.noreply.github.com>
2023-01-16 05:12:28 +0700

    first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import Avatar from "./Avatar";
+
+export function Profile({ person, size, isSepia, thickBorder }) {
+  return (
+    <div className="card">
+      <Avatar
+        person={person}
+        size={size}
+        isSepia={isSepia}
+        thickBorder={thickBorder}
+      />
+    </div>
+  );
+}
